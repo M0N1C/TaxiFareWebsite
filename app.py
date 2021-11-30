9835654d027df682b7f438dd9812205462341716
M0N1C <monica.andres.consulting@gmail.com>
2021-11-30 16:18:18 +0100

    hi

--- app.py
+++ app.py
@@ -8,12 +8,10 @@ import requests
 
 This front queries NVIDIA software 2020'''
 
-pickup_date = st.date_input('Italy',
-                            value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_time = st.time_input('Russia',
-                            value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_datetime = f'{pickup_date} {pickup_time}'
-pickup_longitude = st.number_input('Spain', value=40.7614327)
+italy = st.date_input('Italy')
+russia = st.time_input('Russia')
+#pickup_datetime = f'{pickup_date} {pickup_time}'
+spain = st.number_input('Spain')
 #pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
 #dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
 #dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
@@ -26,17 +24,17 @@ pickup_longitude = st.number_input('Spain', value=40.7614327)
 # enter here the address of your flask api
 url = 'https://taxifare.lewagon.ai/predict'
 
-params = dict(pickup_datetime=pickup_datetime,
-              pickup_longitude=pickup_longitude,
-              pickup_latitude=pickup_latitude,
-              dropoff_longitude=dropoff_longitude,
-              dropoff_latitude=dropoff_latitude,
-              passenger_count=passenger_count)
+#params = dict(pickup_datetime=pickup_datetime,
+#              pickup_longitude=pickup_longitude,
+ #             pickup_latitude=pickup_latitude,
+  #            dropoff_longitude=dropoff_longitude,
+   #           dropoff_latitude=dropoff_latitude,
+    #          passenger_count=passenger_count)
 
-response = requests.get(url, params=params)
+#response = requests.get(url, params=params)
 
-prediction = response.json()
+#prediction = response.json()
 
-pred = prediction['prediction']
+#pred = prediction['prediction']
 
-pred
+#pred
