d979c2ed0d2f1db7ff42837aa6244c6302a21a64
M0N1C <monica.andres.consulting@gmail.com>
2021-12-03 13:40:48 +0100

    hi

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ import streamlit as st
 import requests
 
 '''
-# Is Art a unique human expression?
+# Is art a unique human expression?
 
 This front queries NVIDIA software 2020'''
 
-st.write('Select Country')
+st.write('Select a country')
 
 option_1 = st.checkbox('Italy')
 option_2 = st.checkbox('Russia')
 option_3 = st.checkbox('Spain')
 
 if option_1 or option_2 or option_3:
-    pkl = st.number_input('Choose Pickle and Latent Walk', value=41)
+    #pkl = st.number_input('Choose Pickle', value=41)
+    #pkl = st.number_input('Choose Truncation and Latent Walk', value=0.6)
     left, med1, med2, right = st.columns(4)
     with left:
-        line = st.button('Line')
-    with med1:
-        noise = st.button('Noise')
-    with med2:
-        circular = st.button('Circular')
-    with right:
-        sphere = st.button('Sphere')
+        line = st.button('Generate')
+    #with med1:
+     #   noise = st.button('Noise')
+    #with med2:
+     #   circular = st.button('Circular')
+    #with right:
+     #   sphere = st.button('Sphere')
